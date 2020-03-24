7029685963963b4ef4c655ed1eaf68f6a6506af4
hasib74 <hasibakon74@gmail.com>
2020-03-25 03:11:36 +0600

    AdminApp UI design registation logIn and HomePage

---
+++ lib/src/Display/HomePage.dart
@@ -0,0 +1,308 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SafeArea(
+        child: Scaffold(
+          body: Stack(
+            children: <Widget>[
+              Positioned(
+                child: ClipPath(
+                  clipper: TopWaveClipper(),
+                  child: Container(
+                    height: 250,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          colors: [Color(0xfffbb448), Color(0xfff7892b)]),
+                    ),
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: ClipPath(
+                  clipper: BottomClipper(),
+                  child: Container(
+                    height: 250,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          colors: [Color(0xfffbb448), Color(0xfff7892b)]),
+                    ),
+                  ),
+                ),
+              ),
+              _cards(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  _cards() {
+    return Align(
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Row(
+                children: <Widget>[
+                  _profile(),
+                  SizedBox(
+                    width: 14,
+                  ),
+                  _new_user(),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Row(
+                children: <Widget>[
+                  _payment(),
+                  SizedBox(
+                    width: 14,
+                  ),
+                  _location(),
+                ],
+              ),
+            )
+          ],
+        ));
+  }
+
+  _profile() {
+    return Expanded(
+      flex: 1,
+      child: Stack(
+        children: <Widget>[
+          Container(
+            height: 200,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.black12, spreadRadius: 1.5, blurRadius: 1.5)
+              ],
+              color: Colors.white.withOpacity(0.7),
+            ),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Icon(
+                    Icons.perm_identity,
+                    color: Colors.orange,
+                    size: 100,
+                  ),
+                  Text(
+                    "Profile",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 22,
+                        fontWeight: FontWeight.w500),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          
+          
+          Positioned(
+
+              top: 25,
+              right: 25,
+              child: Text("9+",style: TextStyle(color: Colors.red.withOpacity(0.5),fontSize: 17,fontWeight: FontWeight.bold),))
+          
+        ],
+      ),
+    );
+  }
+
+  _new_user() {
+    return Expanded(
+      flex: 1,
+      child: Container(
+        height: 200,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+          boxShadow: [
+            BoxShadow(color: Colors.black12, spreadRadius: 1.5, blurRadius: 1.5)
+          ],
+          color: Colors.white.withOpacity(0.7),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Icon(
+                Icons.recent_actors,
+                color: Colors.orange,
+                size: 100,
+              ),
+              Text(
+                "New User",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 22,
+                    fontWeight: FontWeight.w500),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  _payment() {
+    return Expanded(
+      flex: 1,
+      child: Container(
+        height: 200,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+          boxShadow: [
+            BoxShadow(color: Colors.black12, spreadRadius: 1.5, blurRadius: 1.5)
+          ],
+          color: Colors.white.withOpacity(0.7),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Icon(
+                Icons.payment,
+                color: Colors.orange,
+                size: 100,
+              ),
+              Text(
+                "Payment",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 22,
+                    fontWeight: FontWeight.w500),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  _location() {
+    return Expanded(
+      flex: 1,
+      child: Container(
+        height: 200,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+          boxShadow: [
+            BoxShadow(color: Colors.black12, spreadRadius: 1.5, blurRadius: 1.5)
+          ],
+          color: Colors.white.withOpacity(0.7),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Icon(
+                Icons.location_on,
+                color: Colors.orange,
+                size: 100,
+              ),
+              Text(
+                "Location",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 22,
+                    fontWeight: FontWeight.w500),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class TopWaveClipper extends CustomClipper<Path> {
+  @override
+  getClip(Size size) {
+    // This is where we decide what part of our image is going to be visible.
+    var path = Path();
+    path.lineTo(0.0, size.height / 3);
+
+    //creating first curver near bottom left corner
+    var firstControlPoint = new Offset(size.width / 3, size.height);
+    var firstEndPoint = new Offset(size.width, size.height);
+
+    path.quadraticBezierTo(firstControlPoint.dx, firstControlPoint.dy,
+        firstEndPoint.dx, firstEndPoint.dy);
+
+    //creating second curver near center
+
+    ///move to top right corner
+    path.lineTo(size.width, 0.0);
+
+    ///finally close the path by reaching start point from top right corner
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    // TODO: implement shouldReclip
+
+    return true;
+  }
+}
+
+class BottomClipper extends CustomClipper<Path> {
+  @override
+  getClip(Size size) {
+    // This is where we decide what part of our image is going to be visible.
+    var path = Path();
+
+    path.lineTo(0.0, 0.0);
+
+    /* var firstControlPoint = new Offset(size.width +30, size.height-100);
+    var firstEndPoint = new Offset(size.width/10, size.height );
+
+    path.quadraticBezierTo(firstControlPoint.dx, firstControlPoint.dy,
+        firstEndPoint.dx, firstEndPoint.dy);*/
+
+    path.quadraticBezierTo(
+        size.width / 2 + 50, size.height / 7, size.width - 20, size.height);
+
+    path.lineTo(size.width, size.height);
+
+    path.lineTo(0.0, size.height);
+
+    ///finally close the path by reaching start point from top right corner
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    // TODO: implement shouldReclip
+
+    return true;
+  }
+}
